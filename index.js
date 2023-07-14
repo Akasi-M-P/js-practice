2b45df28b943cd08c7c701f1b0ecdcc5861c81e9
Peter M. Akasi <118350559+Akasi-M-P@users.noreply.github.com>
2023-07-14 18:57:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//Mark's BMI Calculation
+const markMass = 78;
+const markHeight = 1.69;
 
+const markBMI = markMass / markHeight ** 2;
+
+console.log(markBMI);
+
+//John's BMI Calculation
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const johnBMI = johnMass / johnHeight ** 2;
+
+console.log(johnBMI);
+
+//Comparison of Mark and John BMI
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
