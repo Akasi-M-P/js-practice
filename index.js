b941d979a1b1c8079e79782429439f17da8b486d
Peter M. Akasi <118350559+Akasi-M-P@users.noreply.github.com>
2023-07-15 08:55:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,24 @@ if (driverAge >= 18) {
   const underAge = 18 - driverAge; // Calculating the number of years under the legal driving age
   console.log(`Driver is ${underAge} years under age to drive`); // Printing a message indicating the driver is under age
 }
+
+
+//if/else statement
+
+//Mark's BMI Calculation
+const markMass = 78;
+const markHeight = 1.69;
+
+const markBMI = markMass / markHeight ** 2;
+
+//John's BMI Calculation
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const johnBMI = johnMass / johnHeight ** 2;
+
+if (markBMI > johnBMI) {
+  console.log(`Mark BMI: ${markBMI} is higher than john's BMI: ${johnBMI}`);
+} else {
+  console.log(`John's BMI: ${johnBMI} is higher than Mark's BMI: ${markBMI}`);
+}
